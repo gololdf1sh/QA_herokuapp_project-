02bbbf73988348954d599a7113980c873a38e84d
Oleksandr Kiriukhin <oleksandr.kiriukhin@gmail.com>
2025-03-13 21:58:32 +0200

    Added formAuth.spec.ts formAuthPage.page.ts

---
+++ tests/formAuth/formAuth.spec.ts
@@ -0,0 +1,27 @@
+import { test } from '@playwright/test';
+import { LoginPage } from '../../src/pages/formAuthPage.page';
+
+// Allure метки для організації
+test.describe('@auth @smoke Form Authentication', () => {
+    let loginPage: LoginPage;
+
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page);
+        await loginPage.goto();
+    });
+
+    test('Успішний логін з валідними даними', async () => {
+        await loginPage.login('tomsmith', 'SuperSecretPassword!');
+        await loginPage.assertFlashMessageContains('You logged into a secure area!');
+    });
+
+    test('Невалідний логін повертає помилку', async () => {
+        await loginPage.login('invalidUser', 'SuperSecretPassword!');
+        await loginPage.assertFlashMessageContains('Your username is invalid!');
+    });
+
+    test('Невалідний пароль повертає помилку', async () => {
+        await loginPage.login('tomsmith', 'invalidPassword');
+        await loginPage.assertFlashMessageContains('Your password is invalid!');
+    });
+});
